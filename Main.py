e8c18ac8102e343a6e71b5afd73f5d990ab2cc93
Eatingicecream123 <124335602+Eatingicecream123@users.noreply.github.com>
2023-02-26 14:06:39 -0500

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,148 @@
+#modules
+import time, os, random, sys, colorama
+from colorama import *
+from colorama import init
+os.system('clear')
+
+def slowprint(s):
+	for c in s + '\n':
+		sys.stdout.write(c)
+		sys.stdout.flush()
+		time.sleep(1. / 30)
+
+def slowprint2(s):
+	for c in s + '\n':
+		sys.stdout.write(c)
+		sys.stdout.flush()
+		time.sleep(5. / 30)
+
+
+def Superslowprint(s):
+	for c in s + '\n':
+		sys.stdout.write(c)
+		sys.stdout.flush()
+		time.sleep(15. / 30)
+
+
+print("      █████ ███      █████ █    ██        ███████     ████           █")
+time.sleep(0.2)
+print("   ██████  █ ██   ██████  █  █████      █       ███  █  █████████████ ")
+time.sleep(0.2)
+print("  ██   █  █  ██  ██   █  █     █████   █         ██ █     █████████   ")
+time.sleep(0.2)
+print(" █    █  █   ██ █    █  ██     █ ██    ██        █  █     █  █       ")
+time.sleep(0.2)
+print("     █  █    █  █   █  ███     █        ███          ██  █  ██        ")
+time.sleep(0.2)
+print("    ██ ██   █      ██   ██     █       ██ ███           █  ███        ")
+time.sleep(0.2)
+print("    ██ ██   █      ██   ██     █       ██ ███           █  ███        ")
+time.sleep(0.2)
+print("    ██ ██  █       ██   ██     █        ███ ███        ██   ██        ")
+time.sleep(0.2)
+print("    ██ ████        ██   ██     █          ███ ███      ██   ██        ")
+time.sleep(0.2)
+print("    ██ ██  ███     ██   ██     █            ███ ███    ██   ██        ")
+time.sleep(0.2)
+print("    ██ ██    ██    ██   ██     █              ██ ███   ██   ██        ")
+time.sleep(0.2)
+print("    ██ ██    ██    ██   ██     █              ██ ███   ██   ██        ")
+time.sleep(0.2)
+print("    █  ██    ██     ██  ██     █               ██ ██    ██  ██        ")
+time.sleep(0.2)
+print("       █     ██      ██ █      █                █ █      ██ █      █  ")
+time.sleep(0.2)
+print("       █     ██      ██ █      █                █ █      ██ █      █  ")
+time.sleep(0.2)
+print("   ████      ███      ███      █      ███        █        ███     █   ")
+time.sleep(0.2)
+print("]  █  ████    ██        ████████      █  █████████          ███████   ")
+time.sleep(0.2)
+print(" █    ██     █           ████       █     █████              ███      ")
+time.sleep(0.2)
+print(" █                                  █                                 ")
+time.sleep(0.2)
+print("   ██                                 ██                              ")
+time.sleep(2)
+import replit
+replit.clear()
+
+print("Starting")
+time.sleep(0.5)
+
+import replit
+replit.clear()
+print(' / ')
+time.sleep(0.2)
+
+import replit
+replit.clear()
+print(' - ')
+time.sleep(0.)
+
+import replit
+replit.clear()
+print(' \ ') 
+time.sleep(0.2)
+
+import replit
+replit.clear()
+print(' | ')
+time.sleep(0.2)
+
+import replit
+replit.clear()
+print(' / ')
+time.sleep(0.2)
+
+import replit
+replit.clear()
+print(' - ')
+time.sleep(0.2)
+
+import replit
+replit.clear()
+print(' \ ') 
+time.sleep(0.2)
+
+import replit
+replit.clear()
+print(' | ')
+time.sleep(0.2)
+
+import replit
+replit.clear()
+print(' / ')
+time.sleep(0.2)
+
+import replit
+replit.clear()
+print(' - ')
+time.sleep(0.2)
+
+import replit
+replit.clear()
+print(' \ ') 
+time.sleep(0.2)
+
+import replit
+replit.clear()
+print(' | ')
+time.sleep(0.2)
+import replit
+replit.clear()
+
+
+def RUSTCODES():
+	letters = '1234567890'
+	return ''.join(random.choice(letters) for i in range(4))
+
+
+def genprint(s):
+	for c in s + '\n':
+		sys.stdout.write(c)
+
+genprint(Fore.RED + 'RustCode = ' + Fore.GREEN + RUSTCODES())
+time.sleep(0.5)
+
+slowprint(Fore.LIGHTBLUE_EX + "made by Icecream")
